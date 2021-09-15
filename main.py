5c1f29bd81ec70ad95c25488b21499ecfcb0aeab
Zhanarys Sairanbekov <sairanbekov@taptima.ru>
2021-09-15 18:41:08 +0600

    Init commit

---
+++ main.py
@@ -0,0 +1,17 @@
+arr = input().split()
+
+def all(s):
+   if len(s) == 1:
+     return [s]
+
+   perm_list = []
+   for a in s:
+     remaining_elements = [x for x in s if x != a]
+     z = all(remaining_elements)
+
+     for t in z:
+       perm_list.append([a] + t)
+
+   return perm_list
+
+print(all(arr))
